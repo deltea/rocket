1dc1955a69e75405b11de1e7210f33c9e2c650d6
Leo Feng <thcheetah777@gmail.com>
2024-06-26 13:34:52 -0700

    🏃‍♂️ movement!

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,9 +1,18 @@
 class_name Player extends CharacterBody2D
 
-@export var gravity = 100.0
+@export var gravity = 250.0
+@export var thrust_speed = 400.0
+@export var turn_speed = 150.0
 
 func _physics_process(delta: float) -> void:
 	if !is_on_floor():
 		velocity.y += gravity * delta
+
+	var x_input := Input.get_axis("left", "right")
+	rotation_degrees += x_input * turn_speed * delta
+
+	var thrust_input = Input.is_action_pressed("thrust")
+	if thrust_input:
+		velocity += Vector2.from_angle(rotation - PI / 2) * thrust_speed * delta
 
 	move_and_slide()
