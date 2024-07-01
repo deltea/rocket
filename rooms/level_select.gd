b3d8aaa3c0e8846196242eb01321adc9f9c8eb34
Leo Feng <thcheetah777@gmail.com>
2024-07-01 16:44:26 -0700

    ⭐ add stars

--- rooms/level_select.gd
+++ rooms/level_select.gd
@@ -1,23 +1,34 @@
 class_name LevelSelect extends Room
 
-@export var panel_smoothing = 10.0
 @export var planet_selector_smoothing = 15.0
 @export var camera_look = 0.15
 @export var dotted_line_scene: PackedScene
+@export var parallax_effect = 0.05
+@export var star_texture: Texture2D
+@export var star_num = 100
+@export var camera_smoothing = 10.0
 
-@onready var panel: Panel = $Canvas/Panel
 @onready var planets_parent: Node = $Planets
 @onready var planet_selector: Sprite2D = $PlanetSelector
+@onready var galaxy: TileMap = $Galaxy
+@onready var star_parent: Node2D = $Stars
 
-var panel_target_y = 240.0
 var planet_selector_target: Vector2
+var camera_target_pos = Vector2.ZERO
+var camera_pos = Vector2.ZERO
 
 func _ready() -> void:
 	camera = $Camera
 	planet_selector_target = planets_parent.get_child(0).global_position
 
+	for i in range(star_num):
+		var star = Sprite2D.new()
+		star.texture = star_texture
+		star.global_position = Vector2(randf_range(-160, 160), randf_range(-120, 120 + 320))
+		star_parent.add_child(star)
+
 	for i in range(planets_parent.get_child_count()):
-		if i == planets_parent.get_child_count() - 1: return
+		if i == planets_parent.get_child_count() - 1: continue
 		var planet = planets_parent.get_child(i)
 		var next_planet = planets_parent.get_child(i + 1)
 
@@ -28,13 +39,16 @@ func _ready() -> void:
 		add_child(dotted_line)
 
 func _process(delta: float) -> void:
-	camera.global_position = get_global_mouse_position() * camera_look
+	camera_pos = lerp(camera_pos, camera_target_pos, camera_smoothing * delta)
+	camera.global_position = camera_pos# + get_global_mouse_position() * camera_look
 
-	panel.position.y = lerp(panel.position.y, panel_target_y, panel_smoothing * delta)
+	galaxy.global_position = get_global_mouse_position() * parallax_effect
+	star_parent.global_position = get_global_mouse_position() * parallax_effect * 2
+
 	planet_selector.global_position = lerp(planet_selector.global_position, planet_selector_target, planet_selector_smoothing * delta)
 
 func planet_selected(planet: Planet) -> void:
-	panel_target_y = 0.0
+	camera_target_pos = Vector2(0, 240)
 
 func planet_hovered(planet: Planet):
 	planet_selector_target = planet.global_position
