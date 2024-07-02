fdd1eeb39e33a5b34a6d1b6e14762f7f1ee6ba8d
Leo Feng <thcheetah777@gmail.com>
2024-07-01 18:15:13 -0700

    🐛 centered the grid

--- scenes/grid/grid.gd
+++ scenes/grid/grid.gd
@@ -7,9 +7,10 @@ func _ready() -> void:
 	update_grid()
 
 func update_grid() -> void:
+	var row_amount = ceil(get_child_count() / float(col_amount))
 	for i in range(get_child_count()):
 		var child = get_child(i)
 		var row = i / col_amount
-		var x = i % col_amount * gap
-		var y = row * gap
-		child.global_position = global_position + Vector2(x, y)
+		var x = i % col_amount * gap - (col_amount - 1) * gap / 2
+		var y = row * gap - (row_amount - 1) * gap / 2
+		child.position = Vector2(x, y)
