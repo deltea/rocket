7c123c65a2146a24420d281467e6ed6cf397be11
thcheetah777 <thcheetah777@gmail.com>
2024-07-15 16:35:31 -0700

    🤏 small rocket explode

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -6,7 +6,7 @@ class_name Player extends RigidBody2D
 @export var spawn_y_amount = 18.0
 @export var small_rocket_texture: Texture2D
 
-@onready var thrust_particles: GPUParticles2D = $ThrustParticles
+@onready var thrust_particles: CPUParticles2D = $ThrustParticles
 @onready var sprite: Sprite2D = $Sprite
 @onready var collider: CollisionPolygon2D = $CollisionPolygon
 @onready var landing_area: Area2D = $LandingArea
@@ -58,15 +58,22 @@ func go_into_portal(portal: Portal):
 	tweener.tween_property(self, "global_rotation_degrees", global_rotation_degrees + 360, 1)
 
 func die():
-	var rocket_pieces = rocket_pieces_scene.instantiate()
+	var rocket_pieces = rocket_pieces_scene.instantiate() as Node2D
 	rocket_pieces.global_position = global_position
 	rocket_pieces.rotation = rotation
+
+	if is_small:
+		for child in rocket_pieces.get_children():
+			child.get_node("Sprite").scale = Vector2(0.5, 0.5)
+			child.get_node("CollisionPolygon").scale = Vector2(0.5, 0.5)
+
 	RoomManager.current_room.call_deferred("add_child", rocket_pieces)
 
 	RoomManager.current_room.camera.shake(0.1, 2.0)
 	RoomManager.current_room.reset_dust()
 
 	spawn()
+
 
 func spawn():
 	gravity_scale = 1
@@ -97,8 +104,12 @@ func set_smallness(value: bool):
 	collider.scale = Vector2(0.5, 0.5) if value else Vector2.ONE
 	landing_area.scale = Vector2(0.5, 2.0) if value else Vector2.ONE
 	sprite.texture = small_rocket_texture if value else rocket_texture
-	thrust_particles.position.y = original_particle_pos.y / 2 if value else original_particle_pos.y
 	landing_area.position.y = original_landing_area_pos.y / 2 if value else original_landing_area_pos.y
+	thrust_particles.position.y = original_particle_pos.y / 2 if value else original_particle_pos.y
+	thrust_particles.amount = 6 if value else 12
+	thrust_particles.scale_amount_min = 0.15 if value else 0.3
+	thrust_particles.scale_amount_max = 0.2 if value else 0.4
+	thrust_particles.lifetime = 0.25 if value else 0.5
 
 func _on_body_entered(body: Node):
 	if !can_move: return
